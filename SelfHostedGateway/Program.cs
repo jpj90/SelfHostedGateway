c6beee032e5542ec97f9e87691e8943a8247b0eb
jpj90 <jpauljanse@gmail.com>
2020-11-16 11:30:18 +0100

    configured gateway console app as webhost

--- SelfHostedGateway/Program.cs
+++ SelfHostedGateway/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace SelfHostedGateway
 {
   class Program
   {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      CreateHostBuilder(args).Build().Run();
     }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+      Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+          webBuilder.UseStartup<Startup1>();
+        });
   }
 }
---
+++ SelfHostedGateway/Startup1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace SelfHostedGateway
+{
+  class Startup1
+  {
+    public Startup1(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      //services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.Use(async (context, next) =>
+      {
+        Console.WriteLine($">>>>>>> incoming request: {context.Request.Path}");
+        await next();
+      });
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      //app.UseAuthorization();
+
+      //app.UseEndpoints(endpoints =>
+      //{
+      //  endpoints.MapControllers();
+      //});
+
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync(
+            $"Hello user!");
+      });
+
+    }
+  }
+}
